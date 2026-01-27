e5c114f283ff20c45e0fdcb21be6cd728db1bbb0
CJ-Wilkinson <chancejwilkinson@gmail.com>
2026-01-27 12:50:56 -0700

    Changes made in meeting

--- src/parser/parse.rs
+++ src/parser/parse.rs
@@ -1,9 +1,14 @@
-// ewwwwww
 use chumsky::prelude::Parser;
+use std::collections::HashMap;
 
 #[derive(Debug)]
 struct Name<'src> {
     name: &'src str,
+}
+
+enum Control<'src> {
+	E(Expr<'src>),
+	S(Stmt<'src>),
 }
 
 #[derive(Debug)]
@@ -26,7 +31,7 @@ enum Expr<'src> {
     Var(Box<Name<'src>>),
 
     Call(Box<Name<'src>>, Vec<Expr<'src>>),
-    ArrayExpr(Vec<Expr<'src>>),
+    Array(Vec<Expr<'src>>),
 }
 
 #[derive(Debug)]
@@ -42,7 +47,7 @@ enum Stmt<'src> {
     If(Box<Expr<'src>>, Box<Stmt<'src>>, Option<Box<Stmt<'src>>>),
     Assign(Box<Expr<'src>>, Box<Expr<'src>>),
     ExprStmt(Box<Expr<'src>>),
-    Decl(Box<Type>, Box<Name<'src>>, Box<Expr<'src>>),
+    Decl(Box<Type>, Box<Name<'src>>, Option<Box<Expr<'src>>>),
     Return(Option<Box<Expr<'src>>>),
     Block(Vec<Stmt<'src>>),
     While(Box<Expr<'src>>, Box<Stmt<'src>>),
@@ -59,6 +64,72 @@ struct Fun<'src> {
 struct Program<'src> {
     funs: Vec<Fun<'src>>,
 }
+
+
+pub fn fun_lookup(name: &str) -> Fun {
+	todo!()
+}
+
+pub fn successor_lookup(stmt: &Stmt) -> &Stmt {
+	todo!()
+}
+
+pub fn alloc() -> Address {
+	todo!()
+}
+
+struct Address;
+
+struct Value;
+
+enum Kont<'src> {
+	DeclK(Type, &'src str, &'src Stmt<'src>, &'src Kont),
+}
+
+struct Configuration<'src> {
+	c: &'src Control<'src>,
+	e: HashMap<&'src str, &'src Address>,
+	s: HashMap<&'src Address, &'src Value>,
+	k: &'src Kont,
+}
+
+impl<'src> Configuration<'src> {
+	pub fn next(&self) -> Self {
+		match self.c {
+			Control::S(Stmt::If(condition, t, f)) => todo!(),
+			Control::S(Stmt::Assign(l, r)) => todo!(),
+			Control::S(Stmt::ExprStmt(expr)) => todo!(),
+			Control::S(Stmt::Decl(typ, name, None)) => Self {
+				c: successor_lookup(self.c),
+				e: self.e.clone(),
+				s: self.s,
+				k: self.k,
+			},
+			Control::S(stmt @ Stmt::Decl(typ, name, Some(init))) => {
+				Self {
+					c: Control::E(init),
+					e: self.e,
+					s: self.s,
+					k: Kont::DeclK(typ, name.name, successor_lookup(stmt), self.k),
+				}
+			// let addr = alloc();
+			// Self {
+			// 	c: successor_lookup(self.c),
+			// 	e: self.e.clone().insert(name.name, addr),
+			// 	s: self.s.clone().insert(addr, init),
+			// 	k: self.k,
+			// }
+			},
+			Control::S(Stmt::Return(Some(expr))) => todo!(),
+			Control::S(Stmt::Return(None)) => todo!(),
+			Control::S(Stmt::Block(stmts)) => todo!(),
+			Control::S(Stmt::While(condition, stmt)) => todo!(),
+			Control::S(Stmt::Break) => todo!(),
+			Control::S(Stmt::Continue) => todo!(),
+		}
+	}
+}
+
 
 /*
 pub fn parser<'src>() -> impl Parser<'src, '&src str, Program<'src> {
