2ef5d5e0ba293cb02d2ddc2f0062e0705852c599
Todd Williamson <toddbwilliamson@gmail.com>
2026-01-29 11:56:42 -0700

    added match expression for Expr controls, and refactored successor_lookup function

--- src/main.rs
+++ src/main.rs
@@ -5,5 +5,6 @@ pub mod parser;
 pub use crate::parser::parse;
 
 fn main() {
+    // println!("{}", &std::env::args().nth(1).unwrap());
     parse::parse(&std::env::args().nth(1).unwrap());
 }
--- src/parser/parse.rs
+++ src/parser/parse.rs
@@ -1,17 +1,19 @@
 use chumsky::prelude::Parser;
 use std::collections::HashMap;
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct Name<'src> {
     name: &'src str,
 }
 
+/// The control can be either an expression or a statement
+#[derive(Clone)]
 enum Control<'src> {
     E(Expr<'src>),
     S(Stmt<'src>),
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 enum Value {
     IntV(i32),
     BoolV(bool),
@@ -19,7 +21,10 @@ enum Value {
     ArrayV(Vec<Value>),
 }
 
-#[derive(Debug)]
+/// # Expressions
+/// e := i32 | - (negative) | + | * | - (subtraction) | / | % | == | != | < |  <= | >= | label 
+///     | fn call | []
+#[derive(Debug, Clone)]
 enum Expr<'src> {
     Val(Value),
 
@@ -42,7 +47,7 @@ enum Expr<'src> {
     Array(Vec<Expr<'src>>),
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 enum Type {
     IntT,
     BoolT,
@@ -50,7 +55,11 @@ enum Type {
     ArrayT(Box<Type>),
 }
 
-#[derive(Debug)]
+/// # Statements
+/// s := if | = | expression | declaration (e.g. `int x = 1;`) | return (e)? | {} | while | break
+///     | continue
+
+#[derive(Debug, Clone)]
 enum Stmt<'src> {
     If(Box<Expr<'src>>, Box<Stmt<'src>>, Option<Box<Stmt<'src>>>),
     Assign(Box<Expr<'src>>, Box<Expr<'src>>),
@@ -63,6 +72,8 @@ enum Stmt<'src> {
     Continue,
 }
 
+/// # Function
+/// a function consists of a name, a list of args, and a body statement
 struct Fun<'src> {
     name: Name<'src>,
     args: Vec<(Type, Name<'src>)>,
@@ -77,7 +88,8 @@ pub fn fun_lookup(name: &str) -> Fun {
     todo!()
 }
 
-pub fn successor_lookup(stmt: &Stmt) -> &Stmt {
+// pub fn successor_lookup<'src>(stmt: &'src Stmt) -> &'src Stmt<'src> {
+pub fn successor_lookup<'src>(stmt: &'src Control) -> &'src Control<'src> {
     todo!()
 }
 
@@ -88,15 +100,16 @@ pub fn alloc() -> Address {
 struct Address;
 
 enum Kont<'src> {
-    DeclK(Type, &'src str, &'src Stmt<'src>, &'src Kont),
+        DeclK(Type, &'src str, &'src Stmt<'src>, &'src Kont<'src>),
 }
 
 struct Configuration<'src> {
     c: &'src Control<'src>,
     e: HashMap<&'src str, &'src Address>,
     s: HashMap<&'src Address, &'src Value>,
-    k: &'src Kont,
+    k: &'src Kont<'src>,
 }
+
 
 impl<'src> Configuration<'src> {
     pub fn next(&self) -> Self {
@@ -105,42 +118,69 @@ impl<'src> Configuration<'src> {
             Control::S(Stmt::Assign(l, r)) => todo!(),
             Control::S(Stmt::ExprStmt(expr)) => todo!(),
             Control::S(Stmt::Decl(typ, name, None)) => Self {
-                c: successor_lookup(self.c),
+                c: successor_lookup(self.c), // &Control::S(successor_lookup(stmt)),
                 e: self.e.clone(),
-                s: self.s,
+                s: self.s.clone(),
                 k: self.k,
             },
             Control::S(stmt @ Stmt::Decl(typ, name, Some(init))) => {
-                Self {
-                    c: Control::E(init),
-                    e: self.e,
-                    s: self.s,
-                    k: Kont::DeclK(typ, name.name, successor_lookup(stmt), self.k),
-                }
-                // let addr = alloc();
-                // Self {
-                // 	c: successor_lookup(self.c),
-                // 	e: self.e.clone().insert(name.name, addr),
-                // 	s: self.s.clone().insert(addr, init),
-                // 	k: self.k,
-                // }
-            }
+                todo!()
+                    // Self {
+                    // 	c: Control::E(init),
+                    // 	e: self.e,
+                    // 	s: self.s,
+                    // 	k: Kont::DeclK(typ, name.name, successor_lookup(stmt), self.k),
+                    // }
+                    // let addr = alloc();
+                    // Self {
+                    // 	c: successor_lookup(self.c),
+                    // 	e: self.e.clone().insert(name.name, addr),
+                    // 	s: self.s.clone().insert(addr, init),
+                    // 	k: self.k,
+                    // }
+            },
             Control::S(Stmt::Return(Some(expr))) => todo!(),
             Control::S(Stmt::Return(None)) => todo!(),
             Control::S(Stmt::Block(stmts)) => todo!(),
             Control::S(Stmt::While(condition, stmt)) => todo!(),
             Control::S(Stmt::Break) => todo!(),
             Control::S(Stmt::Continue) => todo!(),
+
+            Control::E(e) => {
+                match e {
+                    Expr::Val(expr) => {
+                        todo!()
+                    },
+                    Expr::Neg(expr) => todo!(),
+                    Expr::Add(expr, y) => todo!(),
+                    Expr::Mult(expr, y) => todo!(),
+                    Expr::Sub(expr, y) => todo!(),
+                    Expr::Div(expr, y) => todo!(),
+                    Expr::Rem(expr, y) => todo!(),
+                    Expr::Eq(expr, y) => todo!(),
+                    Expr::Neq(expr, y) => todo!(),
+                    Expr::Lt(expr, y) => todo!(),
+                    Expr::Gt(expr, y) => todo!(),
+                    Expr::Lte(expr, y) => todo!(),
+                    Expr::Gte(expr, y) => todo!(),
+                    Expr::Var(name) => todo!(),
+                    Expr::Call(name, exprs) => todo!(),
+                    Expr::Array(exprs) => todo!(),
+                }
+            },
         }
     }
 }
 
 /*
-pub fn parser<'src>() -> impl Parser<'src, '&src str, Program<'src> {
-    todo!()
-}
-*/
+   pub fn parser<'src>() -> impl Parser<'src, '&src str, Program<'src> {
+   todo!()
+   }
+   */
 
 pub fn parse(filename: &str) {
     let src = std::fs::read_to_string(filename).unwrap();
+    println!("{src}");
+    let stmts: Vec<_> = src.split_whitespace().collect();
+    println!("stmts: {:?}", stmts);
 }
