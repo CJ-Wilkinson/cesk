ae0ccba9658a7b934da49f6b5486713dbb1122eb
Peter Aldous <peter.aldous@uvu.edu>
2026-02-02 22:32:18 -0700

    fleshing out exprs a bit more

--- src/parser/parse.rs
+++ src/parser/parse.rs
@@ -1,5 +1,5 @@
 use crate::ast::{Expr, Fun, Name, Program, Stmt, Type, Value};
-use chumsky::prelude::{choice, just, text, IterParser, Parser};
+use chumsky::prelude::{choice, just, recursive, text, IterParser, Parser};
 
 pub fn parser<'src>() -> impl Parser<'src, &'src str, Program<'src>> {
     let typ = choice((
@@ -9,20 +9,36 @@ pub fn parser<'src>() -> impl Parser<'src, &'src str, Program<'src>> {
     ));
 
     let int = text::int(10)
-        .map(|s: &str| Expr::Val(Value::IntV(s.parse().unwrap())))
+        .map(|s: &str| Value::IntV(s.parse().unwrap()))
         .padded();
-    let true_v = text::ascii::keyword("true")
-        .padded()
-        .to(Expr::Val(Value::BoolV(true)));
+    let true_v = text::ascii::keyword("true").padded().to(Value::BoolV(true));
     let false_v = text::ascii::keyword("false")
         .padded()
-        .to(Expr::Val(Value::BoolV(false)));
+        .to(Value::BoolV(false));
     let bool_v = choice((true_v, false_v));
 
     let ident = text::ascii::ident().padded().map(|name| Name { name });
 
     let value = choice((int, bool_v));
-    let expr = value;
+    let expr = recursive(|expr| {
+        choice((
+            value.map(|v| Expr::Val(v)),
+            just('-')
+                .ignore_then(expr.clone())
+                .map(|e| Expr::Neg(Box::new(e))),
+            ident
+                .then_ignore(just('('))
+                .then(expr.clone().separated_by(just(',')).collect::<Vec<_>>())
+                .then_ignore(just(')'))
+                .map(|(name, args)| Expr::Call(Box::new(name), args)),
+            // TODO will this get skipped if the previous case tries and fails?
+            ident.map(|n| Expr::Var(Box::new(n))),
+            just('[')
+                .ignore_then(expr.clone().separated_by(just(',')).collect::<Vec<_>>())
+                .then_ignore(just(']'))
+                .map(|exprs| Expr::Array(exprs)),
+        ))
+    });
 
     let stmt = expr
         .then_ignore(just(';'))
