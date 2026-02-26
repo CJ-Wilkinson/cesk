b1b9423ab9ecab057978257e8a4770a6f227ee2f
CJ-Wilkinson <chancejwilkinson@gmail.com>
2026-02-26 16:42:46 -0700

    Changed everything to Reference counters

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,11 @@ use std;
 
 pub mod ast;
 pub mod conf;
-pub mod parser;
+//pub mod parser;
 
-pub use crate::parser::parse;
+//pub use crate::parser::parse;
 
 fn main() {
     // println!("{}", &std::env::args().nth(1).unwrap());
-    parse::parse(&std::env::args().nth(1).unwrap());
+    //parse::parse(&std::env::args().nth(1).unwrap());
 }
