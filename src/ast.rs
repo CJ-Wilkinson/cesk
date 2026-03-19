429f405ed488e5cbfdb2f282bb990a290075d2a6
CJ-Wilkinson <chancejwilkinson@gmail.com>
2026-03-19 15:57:14 -0600

    Remove things

--- src/ast.rs
+++ src/ast.rs
@@ -1,7 +1,4 @@
 pub mod ast;
 
-<<<<<<< Updated upstream
+
 pub use ast::*;
-=======
-pub use ast::{Type, Expr, Name, Operation, Stmt, Value, Arguments, ParamList, Fun, Program};
->>>>>>> Stashed changes
