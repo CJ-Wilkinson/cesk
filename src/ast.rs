b05a5b1c51cb2097e2b1569b266844aadfd3c203
CJ-Wilkinson <chancejwilkinson@gmail.com>
2026-03-19 15:37:55 -0600

    Changes

--- src/ast.rs
+++ src/ast.rs
@@ -1,3 +1,7 @@
 pub mod ast;
 
+<<<<<<< Updated upstream
 pub use ast::*;
+=======
+pub use ast::{Type, Expr, Name, Operation, Stmt, Value, Arguments, ParamList, Fun, Program};
+>>>>>>> Stashed changes
