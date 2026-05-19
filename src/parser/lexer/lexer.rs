65f1c98240d148cef40405bc7937766b6206b7ed
astorie <adstorie07@gmail.com>
2026-05-19 15:02:58 -0600

    Explicit lexer

---
+++ src/parser/lexer/lexer.rs
@@ -0,0 +1,138 @@
+use super::token::*;
+use logos::{Lexer, Logos, Skip};
+
+#[derive(Default, Clone, Copy)]
+struct Position {
+    row: usize,
+    line_start: usize,
+}
+
+#[derive(Logos, Debug, PartialEq)]
+#[logos(extras = Position)]
+enum LexTok {
+    #[regex(r"[A-Za-z_][A-Za-z0-9_]*")]
+    Identifier,
+    #[regex(r"[0-9]+")]
+    IntLiteral,
+    #[regex(r"//[^\r\n]*", logos::skip, allow_greedy = true)]
+    Comment,
+    #[regex(r"==|!=|>=|<=|&&|\|\||[%;{}\[\]\(\)=><!\+\-\*/\,]")]
+    Punctuation,
+    #[regex(r"\r\n|\n|\r", logos::skip)]
+    Whitespace,
+    #[regex(r"[ \t]+", newline_callback)]
+    Newline,
+    #[regex(r"(?s:.)", |lex | lex.slice().chars().next().unwrap(), priority = 0)]
+    //priority set to the lowest, so it's the last possible option
+    Unknown(char),
+}
+
+//update line count and character index
+fn newline_callback(lex: &mut Lexer<LexTok>) -> Skip {
+    lex.extras.row += 1;
+    lex.extras.line_start = lex.span().end;
+    Skip
+}
+
+pub fn lex(src: &str) -> Vec<Token> {
+    let mut out: Vec<Token> = Vec::new();
+    let mut lex = LexTok::lexer(src);
+
+    while let Some(res) = lex.next() {
+        let tok = match res {
+            Ok(tok) => tok,
+            Err(()) => unreachable!(
+                "Error while lexing: Something went very, very wrong. Token completely skipped Unknown tokens."
+            ),
+        };
+
+        let slice = lex.slice();
+        let span = lex.span();
+        let (tag, lexeme) = match tok {
+            LexTok::Identifier => {
+                let tag = keyword_or_identifier(slice);
+                let lx = if tag == TokenTag::ID {
+                    Some(TokenLexeme::Id(slice.to_string()))
+                } else {
+                    None
+                };
+                (tag, lx)
+            }
+            LexTok::IntLiteral => (
+                TokenTag::INTLIT,
+                Some(TokenLexeme::Int(slice.parse::<i64>().unwrap())),
+            ),
+
+            LexTok::Punctuation => {
+                let tag = punctuation(slice);
+                (tag, None)
+            }
+
+            LexTok::Comment | LexTok::Whitespace | LexTok::Newline => continue,
+            LexTok::Unknown(ch) => (TokenTag::UNKNOWN, Some(TokenLexeme::Unknown(ch))),
+        };
+
+        let (row, col) = (
+            lex.extras.row as u32,
+            (span.start - lex.extras.line_start) as u32,
+        );
+
+        out.push(Token::new(tag, span.start, span.end, row, col, lexeme));
+    }
+    out
+}
+
+fn punctuation(s: &str) -> TokenTag {
+    match s {
+        //punctuation / delimitiers
+        ";" => TokenTag::SEMICOLON, //   ;
+        "," => TokenTag::COMMA,     //   ,
+        "{" => TokenTag::LCURLY,    //   {
+        "}" => TokenTag::RCURLY,    //   }
+        "(" => TokenTag::LPAREN,    //   (
+        ")" => TokenTag::RPAREN,    //   )
+        "[" => TokenTag::LSQUARE,   //   [
+        "]" => TokenTag::RSQUARE,   //   ]
+
+        //operators
+        "=" => TokenTag::EQ,     //   =
+        "==" => TokenTag::EQEQ,  //   ==
+        "!=" => TokenTag::NOTEQ, //   !=
+        ">=" => TokenTag::GEQ,   //   >=
+        "<=" => TokenTag::LEQ,   //   <=
+        ">" => TokenTag::GT,     //   >
+        "<" => TokenTag::LT,     //   <
+        "&&" => TokenTag::AND,   //   &&
+        "||" => TokenTag::OR,    //   ||
+        "!" => TokenTag::NOT,    //   !
+        "+" => TokenTag::PLUS,   //   +
+        "-" => TokenTag::MINUS,  //   -
+        "*" => TokenTag::TIMES,  //   *
+        "/" => TokenTag::DIVIDE, //   /
+        "%" => TokenTag::MOD,    //   %
+        _ => TokenTag::UNKNOWN,
+    }
+}
+
+fn keyword_or_identifier(s: &str) -> TokenTag {
+    match s {
+        //literals and identifiers
+        //"" => TokenTag::INTLIT,
+
+        //"" => TokenTag::EOF,
+        //type keywords
+        "bool" => TokenTag::BOOL,   // bool
+        "int" => TokenTag::INT,     // int
+        "unit" => TokenTag::UNIT,   // unit
+        "true" => TokenTag::TRUE,   // true (lowercase)
+        "false" => TokenTag::FALSE, // false (lowercase )
+        //keywords
+        "continue" => TokenTag::CONTINUE, //    continue
+        "if" => TokenTag::IF,             //    if
+        "else" => TokenTag::ELSE,         //    else
+        "while" => TokenTag::WHILE,       //    while
+        "for" => TokenTag::FOR,           //    for
+        "break" => TokenTag::BREAK,       //    break
+        _ => TokenTag::ID,
+    }
+}
---
+++ src/parser/lexer/token.rs
@@ -0,0 +1,161 @@
+use std::fmt;
+use std::fmt::Display;
+
+#[derive(PartialEq, Eq, Clone, Copy, Debug)]
+pub enum TokenTag {
+    //literals and identifiers
+    INTLIT,
+    ID,
+    UNKNOWN,
+    EOF,
+
+    //punctuation / delimitiers
+    SEMICOLON, //   ;
+    COMMA,     //   ,
+
+    LCURLY,  //   {
+    RCURLY,  //   }
+    LPAREN,  //   (
+    RPAREN,  //   )
+    LSQUARE, //   [
+    RSQUARE, //   ]
+
+    //operators
+    EQ,     //   =
+    EQEQ,   //   ==
+    NOTEQ,  //   !=
+    GEQ,    //   >=
+    LEQ,    //   <=
+    GT,     //   >
+    LT,     //   <
+    AND,    //   &&
+    OR,     //   ||
+    NOT,    //   !
+    PLUS,   //   +
+    MINUS,  //   -
+    TIMES,  //   *
+    DIVIDE, //   /
+    MOD,    //   %
+
+    //type keywords
+    BOOL,  // bool
+    INT,   // int
+    UNIT,  // unit
+    TRUE,  // true (lowercase)
+    FALSE, // false (lowercase )
+
+    //keywords
+    CONTINUE, //    continue
+    IF,       //    if
+    ELSE,     //    else
+    WHILE,    //    while
+    FOR,      //    for
+    BREAK,    //    break
+}
+
+impl Display for TokenTag {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{:?}", self)
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub struct TokenSpan {
+    //    ! do we actually need this?
+    //byte offsets (for debugging)
+    pub start: usize,
+    pub end: usize,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub struct TokenLocation {
+    pub row: u32,
+    pub column: u32,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum TokenLexeme {
+    Int(i64),
+    Id(String),
+    Unknown(char),
+}
+
+impl Display for TokenLexeme {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            TokenLexeme::Int(n) => write!(f, "{n}"),
+            TokenLexeme::Id(id) => write!(f, "{id}"),
+            TokenLexeme::Unknown(c) => write!(f, "{c}"),
+        }
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct Token {
+    tag: TokenTag,
+    span: TokenSpan,
+    location: TokenLocation,
+    lexeme: Option<TokenLexeme>,
+}
+
+impl Token {
+    pub fn new(
+        tag: TokenTag,
+        start: usize,
+        end: usize,
+        row: u32,
+        column: u32,
+        lexeme: Option<TokenLexeme>,
+    ) -> Self {
+        Self {
+            tag,
+            span: TokenSpan { start, end },
+            location: TokenLocation { row, column },
+            lexeme,
+        }
+    }
+
+    pub fn kind(&self) -> TokenTag {
+        self.tag
+    }
+
+    pub fn location(&self) -> TokenLocation {
+        self.location
+    }
+
+    pub fn row(&self) -> u32 {
+        self.location.row
+    }
+
+    pub fn column(&self) -> u32 {
+        self.location.column
+    }
+    pub fn span(&self) -> TokenSpan {
+        self.span
+    }
+
+    pub fn start(&self) -> usize {
+        self.span.start
+    }
+
+    pub fn end(&self) -> usize {
+        self.span.end
+    }
+
+    pub fn lexeme(&self) -> Option<&TokenLexeme> {
+        self.lexeme.as_ref()
+    }
+}
+
+impl Display for Token {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let tag = format!("{}", self.tag);
+
+        let lex = self
+            .lexeme
+            .as_ref()
+            .map_or("NONE".to_string(), |lx| lx.to_string());
+
+        write!(f, "{: <20} | {:<4} | {}", tag, self.location.row, lex)
+    }
+}
