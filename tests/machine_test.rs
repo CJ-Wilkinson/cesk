a7a50c2c301e90fcfc4b47093fefb1ea890fb940
astorie <adstorie07@gmail.com>
2026-05-29 14:21:11 -0600

    more test info

--- tests/machine_test.rs
+++ tests/machine_test.rs
@@ -4,7 +4,7 @@ use rust_cesk::conf::ProgramHandler;
 use rust_cesk::conf::conf::Config;
 
 #[test]
-#[should_panic(expected = "Exited with code IntV(1)")]
+#[should_panic(expected = "exited with code IntV(1)")]
 fn runs_basic_program_fixture() {
     let mut program = common::parse_fixture("basic_program.carb");
     let entry = program.get_entry().unwrap();
